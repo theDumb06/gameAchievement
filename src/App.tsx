a0944ebf9e826352ab372bd52c46e3f9541d81b2
theDumb6yo <150143798+theDumb06@users.noreply.github.com>
2026-04-06 10:40:25 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import {AchievementPage} from './AchievementPage'
+
 import './App.css'
 
 function App() {
 
+
   return (
     <>
-      <AchievementPage />
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
