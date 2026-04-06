addefe2b295756fe52c8205cb0e0d2f89cdf82bd
theDumb6yo <150143798+theDumb06@users.noreply.github.com>
2026-04-06 09:53:44 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <>
       <AchievementPage />
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
