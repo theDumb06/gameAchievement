ed8b0bac0069b654f54e5e388e427ce49d199911
theDumb06 <150143798+theDumb06@users.noreply.github.com>
2026-04-14 16:17:19 +0800

    Update SignupPage.tsx

--- src/SignupPage.tsx
+++ src/SignupPage.tsx
@@ -1,15 +1,56 @@
 import loginPageStyle from "./LoginPage.module.css";
+import { useState } from "react";
+import { registerUser } from "./api/auth";
 
 function SignupPage() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const result = await registerUser({
+        username,
+        email,
+        password,
+      });
+
+      console.log("SUCCESS:", result);
+      alert("User registered!");
+    } catch (err) {
+      console.error(err);
+      alert("Error registering user");
+    }
+  };
   return (
-    <div className={loginPageStyle.loginPageContainer}>
-      <div className={loginPageStyle.loginContainer}>
-        <h1>Signup</h1>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button>Sign Up</button>
+    <form onSubmit={handleSubmit}>
+      <div className={loginPageStyle.loginPageContainer}>
+        <div className={loginPageStyle.loginContainer}>
+          <h1>Signup</h1>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button>Register</button>
+        </div>
       </div>
-    </div>
+    </form>
   );
 }
 
