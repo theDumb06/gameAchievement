c681cc9269006dc90f4033a5edc4b538e11bbb85
theDumb06 <150143798+theDumb06@users.noreply.github.com>
2026-04-15 14:43:46 +0800

    Update LoginPage.tsx

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,18 +1,53 @@
 import loginStyles from "./LoginPage.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { login } from "./api/auth";
 
 function LoginPage() {
+
+      const [email, setEmail] = useState("");
+      const [password, setPassword] = useState("");
+    
+      const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+    
+        try {
+          const result = await login(
+            email,
+            password,
+          );
+    
+          console.log("SUCCESS:", result);
+          alert("Logged In!");
+        } catch (err) {
+          console.error(err);
+          alert("Error logging in");
+        }
+      };
+
     return (
+        <form onSubmit={handleSubmit}>
         <div className={loginStyles.loginPageContainer}>
             
             <div className = {loginStyles.loginContainer}>
                 <h1>Login</h1>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
+            <input 
+              type="email" 
+              placeholder="Email" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input 
+              type="password" 
+              placeholder="Password" 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <button>Log In</button>
             <Link to="/signup">Sign Up</Link>
             </div>
         </div>
+        </form>
     );
 }
 
